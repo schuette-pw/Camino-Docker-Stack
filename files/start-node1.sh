16bde240fe2cc2ed9e53bcd3b0e62237c9d594d4
Marcus-Matthias Schütte <marcus.schuette@peakwork.com>
2022-04-25 14:45:12 +0200

    enable debug api and add cchainId

--- files/start-node1.sh
+++ files/start-node1.sh
@@ -3,4 +3,4 @@
 # See the file LICENSE for licensing terms.
 
 node1=$(getent hosts node-1 | awk '{ print $1 }')
-./caminogo --http-host=${node1} --public-ip=${node1} --network-id=local --index-enabled=true --staking-enabled=true --http-port=9650 --staking-port=9651 --staking-tls-cert-file=/files/certs/keys1/staker.crt --staking-tls-key-file=/files/certs/keys1/staker.key --log-level=info
+./caminogo --api-ipcs-enabled=true --chain-config-dir=/files/chain-config --http-host=${node1} --public-ip=${node1} --network-id=local --index-enabled=true --staking-enabled=true --http-port=9650 --staking-port=9651 --staking-tls-cert-file=/files/certs/keys1/staker.crt --staking-tls-key-file=/files/certs/keys1/staker.key --log-level=trace
--- files/start-node5.sh
+++ files/start-node5.sh
@@ -9,4 +9,4 @@ node2=$(getent hosts node-2 | awk '{ print $1 }')
 node3=$(getent hosts node-3 | awk '{ print $1 }')
 node4=$(getent hosts node-4 | awk '{ print $1 }')
 node5=$(getent hosts node-5 | awk '{ print $1 }')
-./caminogo --http-host=${node5} --public-ip=${node5} --network-id=local --index-enabled=true --staking-enabled=true --http-port=9650 --staking-port=9651 --log-level=info --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --bootstrap-ips=${node1}:9651 --staking-tls-cert-file=/files/certs/keys5/staker.crt --staking-tls-key-file=/files/certs/keys5/staker.key
+./caminogo --api-ipcs-enabled=true --chain-config-dir=/files/chain-config --api-admin-enabled=true --http-host=${node5} --network-id=local --index-enabled=true --staking-enabled=true --http-port=9650 --staking-port=9651 --log-level=trace --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --bootstrap-ips=${node1}:9651 --staking-tls-cert-file=/files/certs/keys5/staker.crt --staking-tls-key-file=/files/certs/keys5/staker.key
