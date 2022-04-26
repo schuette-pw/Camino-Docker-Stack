4cfb6001c46edb18255c9125fac1f24616512e1d
Marcus-Matthias Schütte <marcus.schuette@peakwork.com>
2022-04-26 15:55:03 +0200

    fix chain ids and ad shebang in start-node.sh script

--- columbus/files/start-node.sh
+++ columbus/files/start-node.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 indexnode=$(getent hosts index-node | awk '{ print $1 }')
 ./caminogo --api-ipcs-enabled=true --chain-config-dir=/files/chain-config --http-host=${indexnode} --public-ip=${indexnode} --network-id=columbus --index-enabled=true --http-port=9650  --log-level=info
